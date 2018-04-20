1e05db9352af35b05a68097622014d88543099fb
Sperges <rnbiv45@gmail.com>
2018-04-19 20:33:32 -0400

    Add chat box styling

--- src/basicterminal/BasicTerminal.js
+++ src/basicterminal/BasicTerminal.js
@@ -14,14 +14,17 @@ class BasicTerminal extends Component {
             help:'help'
         };
         this.state={
-            lines:[],
-            newline:''
+            termLines:[],
+            newtermline:'',
+            chatLines:[],
+            newchatline:''
         };
         socketApi.subscribeToMessage(
             (message) => {
-                this.writeLines(['Message: ' + message]);
+                this.writeChatLines(['Message: ' + message]);
             });
     }
+
     takeScreenShot = (e) => {
         html2canvas(document.body).then(function(canvas) {
             var img    = canvas.toDataURL("image/png");
@@ -33,66 +36,111 @@ class BasicTerminal extends Component {
             document.body.removeChild(a);
         });
     }
-    updateNewLine = (e) => {
-        //console.log(e.target.value);
-        this.setState({newline:e.target.value});
+
+    updatenewtermline = (e) => {
+        this.setState({newtermline:e.target.value});
     }
-    writeLines = (lines) => {
+
+    updatenewchatline = (e) => {
+        this.setState({newchatline:e.target.value});
+    }
+
+    writeChatLines = (lines) => {
             this.setState({
-                lines:[...this.state.lines, ...lines]
+                chatLines:[...this.state.chatLines, ...lines]
             });
     }
+
+    writeTermLines = (lines) => {
+            this.setState({
+                termLines:[...this.state.termLines, ...lines]
+            });
+    }
+
+
+    submitMessage = (e) => {
+        e.preventDefault();
+        socketApi.sendMessage(this.state.newchatline);
+        this.setState({
+            newchatline:''
+        });
+    }
+
     submitCommand = (e) => {
         e.preventDefault();
-        var commandArray = this.state.newline.split(/(\s+)/).filter( function(e) { return e.trim().length > 0; } );
+        var commandArray = this.state.newtermline.split(/(\s+)/).filter( function(e) { return e.trim().length > 0; } );
         switch(commandArray[0]) {
             case this.commandList.say:
-                this.writeLines([this.pre + this.state.newline]);
+                this.writeTermLines([this.pre + this.state.newtermline]);
                 if (commandArray[1]) {
                     socketApi.sendMessage(commandArray[1]);
                 }
                 break;
             case this.commandList.scrot:
                 this.takeScreenShot();
-                this.writeLines([this.pre + this.state.newline]);
+                this.writeTermLines([this.pre + this.state.newtermline]);
                 break;
             case this.commandList.help:
-                this.writeLines([this.pre + this.state.newline, 'Commands: ', ...Object.keys(this.commandList)]);
+                this.writeTermLines([this.pre + this.state.newtermline, 'Commands: ', ...Object.keys(this.commandList)]);
                 break;
             default:
-                this.writeLines([this.pre + this.state.newline, commandArray[0] + ' is not an operable command']);
+                this.writeTermLines([this.pre + this.state.newtermline, commandArray[0] + ' is not an operable command']);
         }
-        //socketApi.sendMessage(this.state.newline);
         this.setState({
-            newline:''
+            newtermline:''
         });
     }
     render() {
     return (
         <div className="frame">
-            <label htmlFor="command_line">
                 <div className="piece output">
-                    <h1>CONDOR 2049</h1>
-                    <p>Welcome, contestant #F2A44D3E</p>
-                    {
-                        this.state.lines.map(
-                            (line, index) => 
-                            <p key={index}>{line}</p>
-                        )
-                    }
-                    <form onSubmit={this.submitCommand}>
-                    foo@bar:~$ <input autoComplete="off" 
-                                autoCorrect="off" 
-                                autoCapitalize="off" 
-                                spellCheck="false" 
-                                id="command_line" 
-                                type="text" 
-                                className="command_line"
-                                value={this.state.newline}
-                                onChange={this.updateNewLine}/>
-                    </form>
+                    <label htmlFor="command_line">
+                        <div className="term">
+                            <h1>CONDOR 2049</h1>
+                            <p>Welcome, contestant #F2A44D3E</p>
+                            {
+                                this.state.termLines.map(
+                                    (line, index) => 
+                                    <p key={index}>{line}</p>
+                                )
+                            }
+                            <form onSubmit={this.submitCommand}>
+                            foo@bar:~$ <input autoComplete="off" 
+                                        autoCorrect="off" 
+                                        autoCapitalize="off" 
+                                        spellCheck="false" 
+                                        id="command_line" 
+                                        type="text" 
+                                        className="inputArea"
+                                        value={this.state.newtermline}
+                                        onChange={this.updatenewtermline}/>
+                            </form>
+                        </div>
+                    </label>
+                    <div className="chat">
+                        {
+                            this.state.chatLines.map(
+                                (line, index) => 
+                                <p key={index}>{line}</p>
+                            )
+                        }
+                        <form onSubmit={this.submitMessage}>
+                        Say: <input autoComplete="off" 
+                                    autoCorrect="off" 
+                                    autoCapitalize="off" 
+                                    spellCheck="false" 
+                                    wrap="true"
+                                    cols="1"
+                                    id="chat_line" 
+                                    type="text" 
+                                    className="inputArea"
+                                    value={this.state.newchatline}
+                                    onChange={this.updatenewchatline}/>
+                        </form>
+                    </div>
                 </div>
-            </label>
+            
+
             <div className="piece scanlines noclick"></div>
             <div className="piece glow noclick"></div>
         </div>
