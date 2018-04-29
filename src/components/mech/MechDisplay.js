a35e2bc36f58a9508cb688e1a6d23a26b04a36ff
Sperges <rnbiv45@gmail.com>
2018-04-29 05:28:56 -0400

    Fix tree display

--- src/components/mech/MechDisplay.js
+++ src/components/mech/MechDisplay.js
@@ -13,6 +13,46 @@ var mech = {
                     name:"gun",
                     hardpoints:[
                         {name:"Advanced Stabilizer"},
+                        {
+                            name:"Armotron 3000",
+                            hardpoints:[
+                                {
+                                    name:"gun",
+                                    hardpoints:[
+                                        {name:"Advanced Stabilizer"},
+                                        {name:"Advanced Stabilizer"}
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+        {name:"Battery"},
+        {
+            name:"Armotron 3000",
+            hardpoints:[
+                {
+                    name:"gun",
+                    hardpoints:[
+                        {name:"Advanced Stabilizer"},
+                        {name:"Advanced Stabilizer"},
+                        {
+                          name:"Advanced Stabilizer",
+                          hardpoints:[
+                            {name:"Advanced Stabilizer"},
+                            {
+                              name:"Advanced Stabilizer",
+                              hardpoints:[
+                                {name:"Advanced Stabilizer"},
+                                {name:"Advanced Stabilizer"}
+                              ]
+                            }
+                          ]
+                        },
+                        {name:"Advanced Stabilizer"},
+                        {name:"Advanced Stabilizer"},
                         {name:"Advanced Stabilizer"}
                     ]
                 }
@@ -29,59 +69,65 @@ var mech = {
 }
 
 
+
+
 class MechDisplay extends Component{
 
-  isLast(part, parent) {
-    if (parent.hardpoints[parent.hardpoints.length-1] == part) {
-      return "└";
+  isLast(node, parent) {
+    if (parent.hardpoints[parent.hardpoints.length-1] == node) {
+      return true;
     } else {
-      return "├";
+      return false;
     }
   }
 
-  isEnd(part, parent) {
-    if (parent.hardpoints[parent.hardpoints.length-1] == part) {
-      return "";
-    } else {
-      return "│";
+  traverseHelper(rootNode) {
+    var str="";
+    for (let child of rootNode.hardpoints) {
+      str+=this.traverse(child, "", rootNode);
     }
+    return str;
   }
 
-  isParent(part) {
-    if (part.hardpoints) {
-      return "┬ ";
-    } else {
-      return "─ ";
-    }
-  }
-
-  traversePart(part, parent, depth) {
-      if (part.hardpoints) {
-        return (
-          part.hardpoints.map((item, index) => (
-            <div>
-              |{Array(depth).join(" ")}{this.isLast(item,part)}{this.isParent(item)}{item.name}<br/>
-              {this.traversePart(item, part, depth+1)}
-            </div>
-          ))
-        );
+  traverse(node, prefix, parent) {
+      var str="";
+      if (node.hardpoints) {
+        str +=
+          prefix+
+          (this.isLast(node, parent) ? "└┬ " : "├┬ ")+
+          node.name+"\n";
+        for (let child of node.hardpoints) {
+          str += this.traverse(
+            child,
+            prefix+(this.isLast(node, parent) ? " " : "│"),
+            node
+          );
+        }
+      } else {
+        str +=
+          prefix+
+          (this.isLast(node, parent)? "└─ " : "├─ ")+
+          node.name+"\n";
+        if (this.isLast(node, parent)) {
+          str +=prefix+"\n";
+        } else {
+          if(parent === mech) {
+            str +="│\n";
+          }
+        }
       }
+
+      return str;
   }
 
   render(){
     return (
       <div className="game bot">
-      ┌ My Super Cool Mech<br/>
-      │<br/>
-        {
-          mech.hardpoints.map((part, index) => (
-            <div key={index}>
-              {this.isLast(part,mech)}{this.isParent(part)}{part.name}<br/>
-              {this.traversePart(part, mech, 1)}
-              {this.isEnd(part, mech)}
-            </div>
-          ))
-        }
+        <pre>
+          ┌ {mech.name}<br/>
+          │<br/>
+          {this.traverseHelper(mech)}
+        </pre>
       </div>
     );
   }
